09a310389a814a5b79680f39ebd5b4d133b5e7e8
leiqiang <leiq2005@126.com>
2023-08-25 17:12:46 +0800

    add file 'd.sh'

---
+++ d.sh
@@ -0,0 +1,38 @@
+sudo apt-get update
+
+sudo apt-get install -y man
+sudo apt-get install -y tldr
+sudo apt-get install -y vim
+sudo apt-get install -y git
+sudo apt-get install -y make
+sudo apt-get install -y gdb
+sudo apt-get install -y g++
+sudo apt-get install -y tree
+sudo apt-get install -y strace
+sudo apt-get install -y gcc-multilib
+sudo apt-get install -y qemu-system
+sudo apt-get install -y libreadline-dev
+sudo apt-get install -y build-essential
+sudo apt-get install -y libc6-dev-i386
+sudo apt-get install -y gcc-riscv64-linux-gnu
+
+
+# make menuconfig
+sudo apt-get install -y flex
+sudo apt-get install -y bison
+sudo apt-get install -y libncurses5-dev
+sudo apt-get install -y llvm llvm-dev
+
+# SDL2
+sudo apt-get install -y libsdl2
+sudo apt-get install -y libsdl2-dev
+sudo apt-get install -y zlib1g
+sudo apt-get install -y zlib1g-dev
+
+# Python3
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
+pip install z3-solver
+
+
+#sudo apt-get install -y llvm-11 

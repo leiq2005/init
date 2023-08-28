4d5162e252f76b425028919b9946d80f26bffc4d
leiqiang <leiq2005@126.com>
2023-08-28 12:27:03 +0800

    d file

--- d.sh
+++ d.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y vim
 sudo apt-get install -y git
 sudo apt-get install -y make
 sudo apt-get install -y gdb
+sudo apt-get install -y curl 
 sudo apt-get install -y g++
 sudo apt-get install -y tree
 sudo apt-get install -y strace
@@ -16,6 +17,8 @@ sudo apt-get install -y build-essential
 sudo apt-get install -y libc6-dev-i386
 sudo apt-get install -y gcc-riscv64-linux-gnu
 
+#ssh
+sudo apt-get install -y openssh-server
 
 # make menuconfig
 sudo apt-get install -y flex
